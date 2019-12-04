723930ab3e89213a8608d683bc790138670b212f
Vivek <Viv3kdev@gmail.com>
2019-12-04 10:57:00 -0500

    first commit

---
+++ FinalTest/RegisterPage.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterPage.swift
+//  FinalTest
+//
+//  Created by Vivek Mohanan on 2019-12-04.
+//  Copyright © 2019 com.vivekmohanan. All rights reserved.
+//
+
+import UIKit
+
+class RegisterPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
